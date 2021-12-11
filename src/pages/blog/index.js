f37dabd0f4d2dd5c83ff648e4bf7bdc4c661c362
Ryan Schulteis <ryan.schulteis@va.gov>
2021-12-11 11:19:29 -0500

    finished step 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import * as React from 'react'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({ data }) => {
@@ -9,11 +9,12 @@ const BlogPage = ({ data }) => {
       {
         data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
@@ -23,14 +24,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order:DESC}) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        frontmatter{
+        frontmatter {
+          date(formatString: "MMMM D YYYY")
           title
-          date(formatString: "MMMM D, YYYY")
         }
         id
-        body
+        slug
       }
     }
   }
