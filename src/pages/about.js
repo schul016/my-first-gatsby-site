ca7ee53d78f1cf5dd795851ef2228b7f0ac69d15
Ryan Schulteis <ryan.schulteis@va.gov>
2021-12-10 21:19:11 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I like Gatsby, and I made this site with it!</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
